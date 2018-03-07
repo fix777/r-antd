2fdb1f6da52c911b5f2f2e5dcbc2922442e62114
fix777 <wangguoguo.lan@gmail.com>
2018-03-07 15:13:00 +0800

    fix: * missing Table styles

--- components/r-table/style/index.tsx
+++ components/r-table/style/index.tsx
@@ -1,3 +1,8 @@
-import "./index.less";
-
 import "antd/es/table/style/css";
+import "antd/es/card/style/css";
+import "antd/es/button/style/css";
+import "antd/es/popover/style/css";
+import "antd/es/modal/style/css";
+import "antd/es/form/style/css";
+
+import "./index.less";
